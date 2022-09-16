ed61d5135b977e549b0e8438bfce13458e9b2b37
Afranks504 <anthonyfranks504@gmail.com>
2022-09-15 22:19:46 -0800

    Learning basic python

---
+++ HelloWorld.py
@@ -0,0 +1,18 @@
+from ast import If
+
+print("This is my first python file")
+
+y = print("You are incorrect");
+w = print("You are correct");
+
+
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+if 1 > 2:
+    print(y)
+
+if 2 > 1:
+    print(w)
+
